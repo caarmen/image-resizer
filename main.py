8c00a1749192f532cc194d4702313239ec937462
Carmen Alvarez <c@rmen.ca>
2022-07-03 15:18:31 +0200

    Implement forwarding the image from the url (no resizing yet)

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI
-import httpx
-from fastapi.params import Query
+import os
+from tempfile import NamedTemporaryFile
+from urllib.request import urlopen
+
+import uvicorn
+from PIL import Image
+from fastapi import BackgroundTasks, FastAPI
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 
 @app.get("/resize")
-async def resize(image_url: str = Query(None)):
-    async with httpx.AsyncClient() as client:
-        external_response = await client.get(image_url)
-        with open("/tmp/foo", "wb") as file:
-            file.write(external_response.content)
-        return FileResponse("/tmp/foo")
+async def resize(image_url: str, background_tasks: BackgroundTasks):
+    image = Image.open(urlopen(image_url))
+    with NamedTemporaryFile(delete=False) as output_file:
+        image.save(output_file.name, format=image.format)
+        background_tasks.add_task(os.unlink, output_file.name)
+        return FileResponse(output_file.name)
+        # TODO replace temp file with cache
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8100)
