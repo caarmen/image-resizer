d03e2a242905ff0cb42b2624407183d55ffaed41
Carmen Alvarez <c@rmen.ca>
2022-07-03 15:38:16 +0200

    Resize the image

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ from urllib.request import urlopen
 import uvicorn
 from PIL import Image
 from fastapi import BackgroundTasks, FastAPI
+from fastapi.params import Query
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 
 @app.get("/resize")
-async def resize(image_url: str, background_tasks: BackgroundTasks):
+async def resize(background_tasks: BackgroundTasks,
+                 image_url: str,
+                 width: int | None = Query(default=None, gt=0, lt=1024),
+                 height: int | None = Query(default=None, gt=0, lt=1024)):
     image = Image.open(urlopen(image_url))
     with NamedTemporaryFile(delete=False) as output_file:
-        image.save(output_file.name, format=image.format)
+        resized_width = width if width else image.width
+        resized_height = height if height else image.height
+        image_format = image.format
+        image = image.resize((resized_width, resized_height))
+        image.save(output_file.name, format=image_format)
         background_tasks.add_task(os.unlink, output_file.name)
         return FileResponse(output_file.name)
         # TODO replace temp file with cache
