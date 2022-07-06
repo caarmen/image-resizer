f7af76753702f26b11d8731172ac00bb492ee10b
Carmen Alvarez <c@rmen.ca>
2022-07-06 20:29:19 +0200

    Update the purge docker script to specify the right path of the db to purge

--- scripts/docker_purge_images.bash
+++ scripts/docker_purge_images.bash
@@ -1,6 +1,6 @@
 for container in $(docker ps --all --filter ancestor=imageresizer --format="{{.ID}}")
 do
   echo "purging images in container $container"
-  docker exec -it $container python -m imageresizer.purge --max-age ${1:-86400}
+  docker exec --env CACHE_DIR=/var/cache/image-resizer -it $container python -m imageresizer.purge --max-age ${1:-86400}
 done
 
